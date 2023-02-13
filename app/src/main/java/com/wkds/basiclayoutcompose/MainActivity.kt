c815cf145c73a3a7c093bf1f7c542623afd17f8b
Michael Wayne <Kromwell005@gmail.com>
2023-02-13 17:48:36 +0300

    Home Screen scroll added

--- app/src/main/java/com/wkds/basiclayoutcompose/MainActivity.kt
+++ app/src/main/java/com/wkds/basiclayoutcompose/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.wkds.basiclayoutcompose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -21,16 +20,19 @@ import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.wkds.basiclayoutcompose.ui.theme.BasicLayoutComposeTheme
@@ -41,11 +43,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BasicLayoutComposeTheme {
-                //SearchBar()
-                //AlignYourBodyElement()
-
-                //FavoriteCollectionsGrid()
-
                 HomeScreen()
             }
         }
@@ -54,8 +51,8 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun HomeScreen() {
-    Column {
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.verticalScroll(rememberScrollState())) {
         Spacer(modifier = Modifier.height(16.dp))
         SearchBar()
         HomeSection(title = R.string.align_your_body) {
@@ -74,10 +71,20 @@ fun HomeScreen() {
 fun SearchBar(
     modifier: Modifier = Modifier
 ) {
-    TextField(
-        value = "",
 
-        onValueChange = {},
+    val context = LocalContext.current
+
+    var searchBarTxt by remember { mutableStateOf(TextFieldValue()) }
+
+    TextField(
+        value = searchBarTxt,
+
+        onValueChange = {
+            searchBarTxt = it
+            Toast.makeText(context, searchBarTxt.text, Toast.LENGTH_SHORT).show()
+        },
+        maxLines = 1,
+        singleLine = true,
         leadingIcon = {
             Icon(
                 imageVector = Icons.Default.Search,
